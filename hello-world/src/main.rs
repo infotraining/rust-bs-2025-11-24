389a818911adddf5e2bc371c519490ab65fd32db
Krystian Piekos <krystian.piekos@infotraining.pl>
2025-11-24 11:12:55 +0100

    Hello World - done

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,3 +1,32 @@
+use std::io::Write;
+
+#[allow(static_mut_refs)]
+fn input(prompt: &str) -> String {
+    static mut COUNTER: u32 = 0;
+
+    unsafe {
+        COUNTER += 1;
+        println!("Function input() called {} times", COUNTER);
+    }
+
+    print!("{}: ", prompt);
+    std::io::stdout().flush().unwrap();
+
+    let mut value: String = String::new();
+    std::io::stdin().read_line(&mut value).unwrap(); 
+    value.trim().to_string()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let greeting: &str = "Hello, ";
+    
+    let name: String = input("Get name");
+    println!("{}{}!", greeting, name);
+
+    let age_str: String = input("Get your age");
+
+    match age_str.parse::<u32>() {
+        Ok(age) => println!("You are {age} years old."),
+        Err(_) => println!("Invalid age input.")
+    }
 }
